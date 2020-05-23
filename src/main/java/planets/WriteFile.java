2044d62e5214831840b9a9ad9c646f9750082099
stoianandrei9090 <stoianandrei@yahoo.com>
2020-05-23 13:39:01 +0300

    changed to constants in writefile.java

--- src/main/java/planets/WriteFile.java
+++ src/main/java/planets/WriteFile.java
@@ -16,20 +16,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WriteFile {
-    private static final String mercury = "Mercury 0.38709893 0.20563069 7.00487 48.33167 77.45645 252.25084 88.0";
-    private static final String venus = "Venus 0.72333199 0.00677323 3.39471 76.68069 131.53298 181.97973 224.7";
-    private static final String earth = "Earth 1.00000011 0.01671022 0.00005 -11.26064 102.94719 100.46435 365.25";
-    private static final String mars = "Mars 1.52366231 0.09341233 1.85061 49.57854 336.04084 355.45332 687.0";
-    private static final String jupiter = "Jupiter 5.20336301 0.04839266 1.30530 100.55615 14.75385 34.40438 4331.0";
-    private static final String saturn = "Saturn 9.53707032 0.05415060 2.48446 113.71504 92.43194 49.94432 10747.0";
-    private static final String uranus = "Uranus 19.19126393 0.04716771 0.76986 74.22988 170.96424 313.23218 30589.0";
-    private static final String neptune = "Neptune 30.06896348 0.00858587 1.76917 131.72169 44.97135 304.88003 59800.0";
-    static String pluto = "Pluto 39.48168677 0.24880766 17.14175 110.30347 224.06676 238.92881 90560.0";
-    static LocalDateTime epochJ2000 = LocalDateTime.of(2000, 1, 1, 12, 0, 0);
+
+    private static final String MERCURY = "Mercury 0.38709893 0.20563069 7.00487 48.33167 77.45645 252.25084 88.0";
+    private static final String VENUS = "Venus 0.72333199 0.00677323 3.39471 76.68069 131.53298 181.97973 224.7";
+    private static final String EARTH = "Earth 1.00000011 0.01671022 0.00005 -11.26064 102.94719 100.46435 365.25";
+    private static final String MARS = "Mars 1.52366231 0.09341233 1.85061 49.57854 336.04084 355.45332 687.0";
+    private static final String JUPITER = "Jupiter 5.20336301 0.04839266 1.30530 100.55615 14.75385 34.40438 4331.0";
+    private static final String SATURN = "Saturn 9.53707032 0.05415060 2.48446 113.71504 92.43194 49.94432 10747.0";
+    private static final String URANUS = "Uranus 19.19126393 0.04716771 0.76986 74.22988 170.96424 313.23218 30589.0";
+    private static final String NEPTUNE = "Neptune 30.06896348 0.00858587 1.76917 131.72169 44.97135 304.88003 59800.0";
+    private static final String PLUTO = "Pluto 39.48168677 0.24880766 17.14175 110.30347 224.06676 238.92881 90560.0";
+    private static final LocalDateTime EPOCH_J_2000 =
+            LocalDateTime.of(2000, 1, 1, 12, 0, 0);
 
 
     public static void main(String[] args) {
-        String[] strings = {mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto};
+        String[] strings = {MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, PLUTO};
         List<Planet> planetList =  new ArrayList<>();
 
         for(String str : strings) {
@@ -43,7 +45,7 @@ public class WriteFile {
                     meanLongitude = Double.valueOf(words[6]),
                     orbitalPeriod = Double.valueOf(words[7]);
 
-            Planet p = new Planet(name, epochJ2000, inclination, semimajor, ecc, orbitalPeriod,longitudeOfAscending,
+            Planet p = new Planet(name, EPOCH_J_2000 , inclination, semimajor, ecc, orbitalPeriod,longitudeOfAscending,
                     longitudeOfPeriapsis, meanLongitude, PlanetType.PLANET);
             planetList.add(p);
         }
